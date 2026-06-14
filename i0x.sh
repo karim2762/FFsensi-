f066a6a114ef2232434588685792309fbece2795
Kareem Basha <karimullaskmk1234@gmail.com>
2026-06-14 11:25:35 +0530

    Add files via upload

--- i0x.sh
+++ i0x.sh
@@ -1,6 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -eu
 cd "$(dirname "$0")"
+sed -i 's/\r$//' i0x.sh 2>/dev/null || true
+sed -i 's/\r$//' get_sensi.py 2>/dev/null || true
+find x9k -type f -name "*.py" -exec sed -i 's/\r$//' {} \; 2>/dev/null || true
 pkg install -y python 2>/dev/null || true
 mkdir -p o0x
 chmod +x get_sensi.py 2>/dev/null || true
